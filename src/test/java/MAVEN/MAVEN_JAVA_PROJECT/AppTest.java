3378796d8e13d6d086c319a18922f4afaffe20c1
rahulyadav <nakkarahulyadav9@gmail.com>
2024-11-27 14:12:30 +0530

    completed

---
+++ src/test/java/MAVEN/MAVEN_JAVA_PROJECT/AppTest.java
@@ -0,0 +1,19 @@
+package MAVEN.MAVEN_JAVA_PROJECT;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
